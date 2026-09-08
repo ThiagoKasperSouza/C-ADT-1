32b8a30c2eb41418f6ea717d3b30f8116b619309
Thiago Kasper de Souza <164453378+ThiagoKasperSouza@users.noreply.github.com>
2026-09-08 20:09:19 -0300

    Add files via upload

---
+++ point.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "point.h"
+
+ struct point {
+    float x;
+    float y;
+        
+};
+
+Point* create_p(float x, float y) {
+    Point *p = (Point *) malloc(sizeof(Point));
+    if (p != NULL) {
+        p->x = x;
+        p->y = y;
+    }
+    return p;
+};
+
+void access_p(Point *p) {
+    printf("X:%.2f, Y:%.2f\n",p->x,p->y);
+};
+
+void change_p(Point *p, float x, float y) {
+    p->x = x;
+    p->y = y;
+}
+
+void free_p(Point *p) {
+    free(p);
+    p = NULL;   
+};
+
+float dist_btw_2_p(Point *p1, Point *p2){
+    float x_squared = pow((p2->x-p1->x),2.0);
+    float y_squared = pow((p2->y-p1->y),2.0);
+    float d = sqrt(x_squared+y_squared);
+    return d;
+};
